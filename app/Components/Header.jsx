25d9460e8356afe589813a0a325abe348dd19b31
junaeidy <junaeidys0@gmail.com>
2025-02-23 20:41:44 +0700

    change header

--- app/Components/Header.jsx
+++ app/Components/Header.jsx
@@ -44,7 +44,7 @@ const Header = () => {
                 initial={{y:30, opacity:0}}
                 whileInView={{y:0, opacity:1}}
                 transition={{duration: 0.6, delay: 1.2}}
-            href="/sample-resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 dark:text-black bg-white'>My Resume<Image src={assets.download_icon} alt='' className='w-4'/>
+            href="" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 dark:text-black bg-white'>My Resume<Image src={assets.download_icon} alt='' className='w-4'/>
             </motion.a>
         </div>
     </div>
